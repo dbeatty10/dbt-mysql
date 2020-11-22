a684ceadfee26430ad5aed6300a6ee670eff7fb6
Doug Beatty <doug.beatty@gmail.com>
2020-11-22 08:46:29 -0700

    Remove commented code from MySQLColumn

--- dbt/adapters/mysql/column.py
+++ dbt/adapters/mysql/column.py
@@ -16,24 +16,9 @@ class MySQLColumn(Column):
     table_stats: Optional[Dict[str, Any]] = None
     column_index: Optional[int] = None
 
-    # @classmethod
-    # def translate_type(cls, dtype: str) -> str:
-    #     return dtype
-    #
-    # def can_expand_to(self: Self, other_column: Self) -> bool:
-    #     """returns True if both columns are strings"""
-    #     return self.is_string() and other_column.is_string()
-    #
-    # def literal(self, value):
-    #     return "cast({} as {})".format(value, self.dtype)
-
     @property
     def quoted(self) -> str:
         return '`{}`'.format(self.column)
-
-    # @property
-    # def data_type(self) -> str:
-    #     return self.dtype
 
     def __repr__(self) -> str:
         return "<MySQLColumn {} ({})>".format(self.name, self.data_type)
