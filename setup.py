74bc3d402ccfc3dcc7b31aae60d30af532e7d529
Doug Beatty <doug.beatty@gmail.com>
2020-12-30 08:06:48 -0700

    Replace pyodbc with mysql-connector-python

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
     },
     install_requires=[
         "dbt-core==0.19.0rc1",
-        "pyodbc",
+        "mysql-connector-python~=8.0.22",
     ],
     classifiers=[
         'Development Status :: 3 - Alpha',
