7e2ec93ce496320fafd3e62cac0b7539f48ea6f8
Doug Beatty <doug.beatty@gmail.com>
2020-12-31 15:45:32 -0700

    Split the mysql5 adapter from the original mysql adapter

---
+++ dbt/include/mysql5/macros/adapters.sql
@@ -0,0 +1,95 @@
+
+{% macro mysql5__list_schemas(database) %}
+    {% call statement('list_schemas', fetch_result=True, auto_begin=False) -%}
+        select distinct schema_name
+        from information_schema.schemata
+    {%- endcall %}
+
+    {{ return(load_result('list_schemas').table) }}
+{% endmacro %}
+
+{% macro mysql5__create_schema(relation) -%}
+  {%- call statement('create_schema') -%}
+    create schema if not exists {{ relation.without_identifier() }}
+  {%- endcall -%}
+{% endmacro %}
+
+{% macro mysql5__drop_schema(relation) -%}
+  {%- call statement('drop_schema') -%}
+    drop schema if exists {{ relation.without_identifier() }}
+  {% endcall %}
+{% endmacro %}
+
+{% macro mysql5__drop_relation(relation) -%}
+    {% call statement('drop_relation', auto_begin=False) -%}
+        drop {{ relation.type }} if exists {{ relation }}
+    {%- endcall %}
+{% endmacro %}
+
+{% macro mysql5__truncate_relation(relation) -%}
+    {% call statement('truncate_relation') -%}
+      truncate table {{ relation }}
+    {%- endcall %}
+{% endmacro %}
+
+{% macro mysql5__create_table_as(temporary, relation, sql) -%}
+  {%- set sql_header = config.get('sql_header', none) -%}
+
+  {{ sql_header if sql_header is not none }}
+
+  create {% if temporary: -%}temporary{%- endif %} table
+    {{ relation.include(database=False) }}
+    {{ sql }}
+{% endmacro %}
+
+{% macro mysql5__current_timestamp() -%}
+  current_timestamp()
+{%- endmacro %}
+
+{% macro mysql5__rename_relation(from_relation, to_relation) -%}
+  {#
+    MySQL rename fails when the relation already exists, so a 2-step process is needed:
+    1. Drop the existing relation
+    2. Rename the new relation to existing relation
+  #}
+  {% call statement('drop_relation') %}
+    drop {{ to_relation.type }} if exists {{ to_relation }} cascade
+  {% endcall %}
+  {% call statement('rename_relation') %}
+    rename table {{ from_relation }} to {{ to_relation }}
+  {% endcall %}
+{% endmacro %}
+
+{% macro mysql5__check_schema_exists(database, schema) -%}
+    {# no-op #}
+    {# see MySQLAdapter.check_schema_exists() #}
+{% endmacro %}
+
+{% macro mysql5__get_columns_in_relation(relation) -%}
+    {% call statement('get_columns_in_relation', fetch_result=True) %}
+        show columns from {{ relation.schema }}.{{ relation.identifier }}
+    {% endcall %}
+
+    {% set table = load_result('get_columns_in_relation').table %}
+    {{ return(sql_convert_columns_in_relation(table)) }}
+{% endmacro %}
+
+{% macro mysql5__list_relations_without_caching(schema_relation) %}
+  {% call statement('list_relations_without_caching', fetch_result=True) -%}
+    select
+      null as "database",
+      table_name as name,
+      table_schema as "schema",
+      case when table_type = 'BASE TABLE' then 'table'
+           when table_type = 'VIEW' then 'view'
+           else table_type
+      end as table_type
+    from information_schema.tables
+    where table_schema = '{{ schema_relation.schema }}'
+  {% endcall %}
+  {{ return(load_result('list_relations_without_caching').table) }}
+{% endmacro %}
+
+{% macro mysql5__generate_database_name(custom_database_name=none, node=none) -%}
+  {% do return(None) %}
+{%- endmacro %}
