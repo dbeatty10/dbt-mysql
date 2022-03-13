f96ef0a6c99b7bb01a05e6fb3d1fceb0972bbc8a
Doug Beatty <doug.beatty@gmail.com>
2022-03-13 14:10:04 -0600

    Support dbt v0.20.2

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ except ImportError:
 
 
 package_name = "dbt-mysql"
-package_version = "0.20.1"
+package_version = "0.20.2"
 description = """The MySQL adapter plugin for dbt (data build tool)"""
 
 this_directory = os.path.abspath(os.path.dirname(__file__))
